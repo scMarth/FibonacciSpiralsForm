b78a8d50fcb7f781c08fc72a262e9b2b929b1d75
scMarth <vlantaca@gmail.com>
2018-02-16 14:33:33 -0800

    first commit

---
+++ FibonacciSpirals/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace FibonacciSpirals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.graph1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radiusTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.originTextBoxY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.originTextBoxX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.graph1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graph1
+            // 
+            this.graph1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.graph1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.graph1.Legends.Add(legend2);
+            this.graph1.Location = new System.Drawing.Point(12, 70);
+            this.graph1.Name = "graph1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.MarkerColor = System.Drawing.Color.Blue;
+            series3.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series3.Name = "Points";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.MarkerColor = System.Drawing.Color.Red;
+            series4.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series4.Name = "Origin";
+            this.graph1.Series.Add(series3);
+            this.graph1.Series.Add(series4);
+            this.graph1.Size = new System.Drawing.Size(720, 517);
+            this.graph1.TabIndex = 0;
+            this.graph1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(592, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Export to Text";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(592, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Generate Preview";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radiusTextBox
+            // 
+            this.radiusTextBox.Location = new System.Drawing.Point(286, 11);
+            this.radiusTextBox.Name = "radiusTextBox";
+            this.radiusTextBox.Size = new System.Drawing.Size(68, 20);
+            this.radiusTextBox.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(210, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Radius";
+            // 
+            // originTextBoxY
+            // 
+            this.originTextBoxY.Location = new System.Drawing.Point(98, 41);
+            this.originTextBoxY.Name = "originTextBoxY";
+            this.originTextBoxY.Size = new System.Drawing.Size(68, 20);
+            this.originTextBoxY.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "X";
+            // 
+            // originTextBoxX
+            // 
+            this.originTextBoxX.Location = new System.Drawing.Point(98, 9);
+            this.originTextBoxX.Name = "originTextBoxX";
+            this.originTextBoxX.Size = new System.Drawing.Size(68, 20);
+            this.originTextBoxX.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Origin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 599);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radiusTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.originTextBoxY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.originTextBoxX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.graph1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.graph1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart graph1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox radiusTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox originTextBoxY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox originTextBoxX;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FibonacciSpirals/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonacciSpirals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double tau = (1 + Math.Sqrt(5)) / 2;
+
+            double[] temp = new double[400];
+            double[] theta = new double[400];
+            double[] rad = new double[400];
+
+            double[] deltaX = new double[400];
+            double[] deltaY = new double[400];
+
+            double[] ptsX = new double[400];
+            double[] ptsY = new double[400];
+
+            double origX = 508000;
+            double origY = 4475000;
+
+            double minx = 0, maxx = 0, miny = 0, maxy = 0;
+
+            // Add the origin
+            graph1.Series[1].Points.AddXY(origX, origY);
+
+            for (int i=1; i<=400; i++)
+            {
+                temp[i-1] = i;
+                theta[i-1] = temp[i-1]*(2*Math.PI)/tau;
+                rad[i - 1] = Math.Sqrt(temp[i - 1]);
+
+                deltaX[i - 1] = (Math.Cos(theta[i - 1])) * rad[i - 1];
+                deltaY[i - 1] = (Math.Sin(theta[i - 1])) * rad[i - 1];
+
+                ptsX[i - 1] = deltaX[i - 1] + origX;
+                ptsY[i - 1] = deltaY[i - 1] + origY;
+
+                if (i == 1)
+                {
+                    minx = ptsX[i-1];
+                    maxx = ptsX[i - 1];
+                    miny = ptsY[i - 1];
+                    maxy = ptsY[i - 1];
+                }else
+                {
+                    if (ptsX[i - 1] < minx) minx = ptsX[i - 1];
+                    if (ptsX[i - 1] < maxx) maxx = ptsX[i - 1];
+                    if (ptsY[i - 1] < miny) miny = ptsY[i - 1];
+                    if (ptsY[i - 1] < maxy) maxy = ptsY[i - 1];
+                }
+
+                graph1.Series[0].Points.AddXY(ptsX[i-1], ptsY[i-1]);
+                graph1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            }
+
+            //textBox1.Text = "minx = " + minx.ToString() + " | maxx = " + maxx.ToString() + " | miny = " + miny.ToString() + " | maxy = " + maxy.ToString();
+
+            graph1.ChartAreas[0].AxisX.IsStartedFromZero = false;
+            graph1.ChartAreas[0].AxisY.IsStartedFromZero = false;
+
+            // Set automatic zooming
+            graph1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            graph1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+
+            // Set automatic scrolling 
+            graph1.ChartAreas[0].CursorX.AutoScroll = true;
+            graph1.ChartAreas[0].CursorY.AutoScroll = true;
+
+            // Allow user selection for Zoom
+            graph1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            graph1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+
+        }
+    }
+}
